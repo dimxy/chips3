f6be447846c2c0c845a7d98eea41a7d52dd9260d
dimxy <dimxy@komodoplatform.com>
2021-06-25 09:45:32 +0500

    added random entropy to testdice, removed extra generate, path to chips-cli fixed

--- test/testdice.py
+++ test/testdice.py
@@ -1,8 +1,9 @@
-# test 
+# test dice
 import os
 import time
 import json
 import subprocess
+import random
 from subprocess import check_output as go
 
 
@@ -12,17 +13,25 @@ def run_dice() :
 # dicebet 0243692eb833239fde48b00fc3d7d62f89afeb7e435ce41a3271adb3460ef85c20 031f89c2f89f298300afb3952675f9a024e7302252ef43b6fa4d5d66316f5864e1 af9ed0f9aad3254ab7ea3ff20c4382556447c0d25008b978f023730190839166 1 1:1
 # diceclaim b 0243692eb833239fde48b00fc3d7d62f89afeb7e435ce41a3271adb3460ef85c20 01e38b2f79f28fc748de4852edbe8c566c46c9056436be9011066148ec4b6206 00266c0463a23f349372f8932c0fa8e3ee6287551ce55e9dde80fd1ed653079b c5eb48939e860ee7a0e71df192be7e7f675ae587a4b4ba999df086d60d56191c:1 a43483299d9038ba86bb8610d012e0a63afe6be67b8de314941a2c0245ddcc07:0
 
+    random.seed()
+
+    basecmd = "../src/chips-cli" 
+
     house_pk = "031f89c2f89f298300afb3952675f9a024e7302252ef43b6fa4d5d66316f5864e1" # "021057af9921d92518af8b38b34ec07069a53c7f819f616571de4f2aaffb162449"
     bettor_pk =  "0243692eb833239fde48b00fc3d7d62f89afeb7e435ce41a3271adb3460ef85c20" # "031ed7c820749e724eea5b74a1047c1b7cabdb66543b7568722f6daff06058af54"
-    house_ent = "01e38b2f79f28fc748de4852edbe8c566c46c9056436be9011066148ec4b6206"
-    bettor_ent = "00266c0463a23f349372f8932c0fa8e3ee6287551ce55e9dde80fd1ed653079b"
-    house_hash = "6d92705c876fc10034f1abba70ffac294df040d288ac82534675ec86b0fab62a"
-    bettor_hash = "af9ed0f9aad3254ab7ea3ff20c4382556447c0d25008b978f023730190839166"
-    house_amount = "2"
-    bettor_amount = "2"
-    odds = "100:1"
-
-    basecmd = "/Users/dimxy/repo/chips3/src/chips-cli" 
+    house_ent = hex(random.randint(1, 0x7fff_ffff))[2:].zfill(64)
+    bettor_ent = hex(random.randint(1, 0x7fff_ffff))[2:].zfill(64)    
+    print("house_ent=", house_ent)
+    print("bettor_ent=", bettor_ent)
+    output = go([basecmd, "-testnet", "dicehentropy", house_ent])
+    house_hash = output.decode("utf-8").strip()
+    output = go([basecmd, "-testnet", "dicehentropy", bettor_ent])
+    bettor_hash = output.decode("utf-8").strip()
+    print("house_hash=", house_hash)
+    print("bettor_hash=", bettor_hash)
+    house_amount = "0.2"
+    bettor_amount = "0.2"
+    odds = "1:1"
 
     print("funding house, amount:", house_amount)
     output = go([basecmd, "-testnet", "dicefund", house_pk, bettor_pk, house_hash, house_amount])
@@ -34,7 +43,7 @@ def run_dice() :
     txidbettor = output.decode("utf-8").strip()
     print("dicebet txid=", txidbettor)
 
-    go([basecmd, "-testnet", "generate", "1", "100000000"])  
+    ## go([basecmd, "-testnet", "generate", "1", "100000000"])  
 
     # find vout with funds
     output = go([basecmd, "-testnet", "getrawtransaction", txidhouse, "true"])
@@ -62,7 +71,7 @@ def run_dice() :
         claimres = output.decode("utf-8").strip()
         print("diceclaim as house result:", claimres)
         if len(claimres) == 64 :   # returned non error but txid
-            go([basecmd, "-testnet", "generate", "1"])  
+            go([basecmd, "-testnet", "generate", "1", "100000000"])  
             output = go([basecmd, "-testnet", "getrawtransaction", claimres, "true"])
             strjson = output.decode("utf-8").strip()
             txjson = json.loads(strjson)
@@ -79,7 +88,7 @@ def run_dice() :
         claimres = output.decode("utf-8").strip()
         print("diceclaim as bettor result:", claimres)
         if len(claimres) == 64 :   # returned non error but txid
-            go([basecmd, "-testnet", "generate", "1"])  
+            go([basecmd, "-testnet", "generate", "1", "100000000"])  
             output = go([basecmd, "-testnet", "getrawtransaction", claimres, "true"])
             strjson = output.decode("utf-8").strip()
             txjson = json.loads(strjson)
